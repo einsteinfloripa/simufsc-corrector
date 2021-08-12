e12dc9b63bf78cdf71a88951542391f7fce02593
Murillo Stein <murillo.stein8@gmail.com>
2021-08-12 20:42:38 -0300

    atualizando relatorio docentes

---
+++ relatorios/relatorio-docentes.py
@@ -0,0 +1,215 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import PIL
+# import matplotlib.pyplot as plt
+
+st.title('Relatório Docentes')
+
+st.write('Olá Docente! Este é o seu **relatório do SIMUFSC 2021**.')
+st.write("Ele é dividido em **duas seções principais**. A primeira delas, apresenta dados gerais sobre toda a prova, estas informações são padronizadas para todas as disciplinas. A seção 2 apresenta dados **específicos para cada matéria**. Para selecionar qual matéria você deseja analisar os dados, basta selecioná-la na barra lateral esquerda.")
+st.write("Façam bom uso!")
+
+# ! dados gerais !
+
+st.header("**1. Dados sobre a Prova Geral**")
+
+st.subheader("**1.1 Dados sobre as Questões Objetivas**")
+
+# media e % das objetivas 
+media_acerto_materia = pd.read_pickle('relatorios/dados/docentes/media_acerto_materia.pkl')
+#media_acertos_obj = media_acerto_materia['Correção'].mean()
+
+#st.write("A média geral de acertos da prova objetiva foi ", str(round(media_acertos_obj, 3)*100)+'%')
+ 
+
+st.markdown("**Média de acertos por matéria (em %)**")
+
+media_acerto_materia = media_acerto_materia.reset_index()
+
+# para nao mostrar on indices do dataframe na tabela do streamlit, podemos mudar o indice
+media_acerto_materia.set_index('Disciplina',inplace=True)
+
+st.write(pd.DataFrame({
+   'Média de acertos': round(media_acerto_materia['Média']*100, 2)
+}))
+
+
+def get_media_a_m():
+    path = 'relatorios/dados/docentes/media_acerto_materia.pkl'
+    return pd.read_pickle(path)
+ 
+# gráfico com a média de acerto de cada matéria
+
+# o grafico fica com os rotulos na horizontal com as duas linhas a seguir, mas, quando o codigo é upado no servidor do streamlit ele sai todo desconfigurado. 
+# caso queira testar para ver se não dá mais o bug, é so "descomentar" as duas linhas a seguir
+
+# media_acerto_materia = media_acerto_materia.reset_index()
+# media_acerto_materia = media_acerto_materia.set_index('Correção')
+media_acerto_materia = pd.read_pickle('relatorios/dados/docentes/media_acerto_materia.pkl')
+st.markdown("**Gráfico da porcentagem média de acerto em cada matéria**")
+st.bar_chart(data=media_acerto_materia["Média"])
+
+st.write("Passando o mouse por cima do gráfico você pode identificar qual é a matéria e nota referentes a cada barra.")
+
+
+# media e % da redação
+
+media_redacao =  pd.read_pickle('relatorios/dados/docentes/media_redacao.pkl')
+print(media_redacao)
+# tabela com os dados coletados acima
+st.subheader("**1.2 Dados sobre a Redação**")
+st.write(pd.DataFrame({
+    'Nota Média': media_redacao['Nota']
+}))
+
+# questões discursivas
+
+
+st.subheader("**1.3 Dados sobre as Questões discursivas**")
+
+st.write("Os dados dizem respeito à pontuação média obtida em cada alternativa e na nota final.")
+media_discursivas = pd.read_pickle('relatorios/dados/docentes/media_discursivas.pkl')
+st.write(pd.DataFrame({
+    'A': media_discursivas['a)'],
+    'B': media_discursivas['b)'],
+    "C": media_discursivas['c)'],
+    'Nota final': media_discursivas['Nota final']
+}))
+
+
+# tabela com a colocação dos alunos
+
+colocacao = pd.read_pickle('relatorios/dados/docentes/colocacao.pkl')
+st.subheader("**1.4   Colocação dos alunos do Einstein**")
+colocacao.set_index('Colocação',inplace=True)
+st.write(pd.DataFrame({
+    'Nome': colocacao['aluno_login'],
+    'Pontos': colocacao['Pontuação'],
+    "Porcentagem de Acerto": colocacao['% de acerto']
+}))
+
+
+#  ! dados específicos por matéria  ! 
+
+# questões separadas por materia e por assunto
+# # tem o total de acertos, total de alunos que responderam e média de acerto
+
+media_acerto_materia = get_media_a_m()
+media_acerto_materia = media_acerto_materia.reset_index()
+
+materia_escolhida = st.sidebar.selectbox("Escolha a matéria", media_acerto_materia['Disciplina'])
+
+dados_materia_escolhida = media_acerto_materia[(media_acerto_materia['Disciplina'] == materia_escolhida)]
+
+# para nao mostrar on indices do dataframe na tabela do streamlit, podemos mudar o indice
+dados_materia_escolhida.set_index('Disciplina',inplace=True)
+
+st.header("**2. Dados sobre a Matéria Selecionada**")
+
+st.write("Aqui você encontra dados sobre a matéria que foi selecionada na caixa localizada na aba lateral.")
+st.write(pd.DataFrame({
+    "Média Acertos(em %)": dados_materia_escolhida['Média']*100
+}))
+
+
+# todas as questões separadas por matéria
+# #  tem o total de acertos, total de alunos que responderam e média de acerto
+media_acerto_questao = pd.read_pickle('relatorios/dados/docentes/media_acerto_questao.pkl')
+
+st.subheader("**2.1   Média de acertos por questao**")
+st.write("Nesta seção são discretizados os acertos por questão")
+st.markdown("Como foram aplicadas duas provas com duas ordens de questões diferentes, há a possibilidade de os dados de uma mesma questão estarem separados em duas partes.")
+media_acerto_questao = media_acerto_questao.reset_index()
+questao_materia_escolhida = media_acerto_questao[(media_acerto_questao['Disciplina'] == materia_escolhida)]
+
+
+# para nao mostrar on indices do dataframe na tabela do streamlit, podemos mudar o indice
+questao_materia_escolhida.set_index('Questão',inplace=True)
+
+
+
+st.write(pd.DataFrame({
+    "Assunto": questao_materia_escolhida['TEMA'],
+    "Dificuldade": questao_materia_escolhida['NÍVEL'],
+    "Média Acertos(em %)": questao_materia_escolhida['Média']*100,
+    "Tempo Médio(minutos)": questao_materia_escolhida["tempo_no_exercicio(s)"]/60
+}))
+
+grafico_questoes = questao_materia_escolhida['Média']
+grafico_questoes = grafico_questoes.reset_index()
+grafico_questoes['questao'] = grafico_questoes['Questão'].astype(str)
+grafico_questoes["media"] = grafico_questoes['Média']
+grafico_questoes.set_index('questao',inplace=True)
+
+st.bar_chart(data=grafico_questoes['media'])
+st.write("Dependendo das pontuações das questões, pode ser que o gráfico apresente **escalas diferentes para cada matéria**. Lembre de sempre olhar os valores do eixo vertical.")
+
+# questões separadas por materia e por assunto
+# #  tem o total de acertos, total de alunos que responderam e média de acerto
+
+def get_media_p_a():
+    path = 'relatorios/dados/docentes/media_por_assunto.pkl'
+    return pd.read_pickle(path)
+media_por_assunto = get_media_p_a()
+# print(media_por_assunto)
+
+media_por_assunto = media_por_assunto.reset_index()
+assuntos_materia_escolhida = media_por_assunto[(media_por_assunto['Disciplina'] == materia_escolhida)]
+
+# para nao mostrar on indices do dataframe na tabela do streamlit, podemos mudar o indice
+assuntos_materia_escolhida.set_index('TEMA',inplace=True)
+
+st.subheader("**2.2   Média de acertos por assunto**")
+
+st.write(pd.DataFrame({
+    "Total Acertos": assuntos_materia_escolhida['Total Acertos'],
+    'Total Respostas': assuntos_materia_escolhida['Total Respostas'],
+    'Média (em %)': round(assuntos_materia_escolhida['Média']*100,3)
+}))
+
+
+
+# analise dos acertos por dificuldade
+media_por_dificuldade = pd.read_pickle('relatorios/dados/docentes/media_por_dificuldade.pkl')
+
+st.subheader("**2.3   Média de acertos por dificuldade**")
+
+media_por_dificuldade = media_por_dificuldade.reset_index()
+dificuldade_materia_escolhida = media_por_dificuldade[(media_por_dificuldade['Disciplina'] == materia_escolhida)]
+
+# para nao mostrar on indices do dataframe na tabela do streamlit, podemos mudar o indice
+dificuldade_materia_escolhida.set_index('NÍVEL',inplace=True)
+
+
+st.write(pd.DataFrame({
+    "Total Acertos": dificuldade_materia_escolhida['Total Acertos'],
+    'Total Respostas': dificuldade_materia_escolhida['Total Respostas'],
+    'Média (em %)': round(dificuldade_materia_escolhida['Média']*100,3)		
+}))
+
+
+# destrinchando as alternativas que os alunos assinalaram 
+
+st.subheader("**2.4   Distribuição das respostas dos alunos**")
+
+st.write("Aqui você encontra a distibuição de vezes que os alunos assinalaram as alternativas. Por exemplo, 14/60 significa que dos 60 alunos que responderam a questão, 14 julgaram a alternativa como correta")
+
+qntd_assinaladas = pd.read_pickle("relatorios\dados\docentes\qntd_assinaladas.pkl")
+qntd_assinaladas.reset_index(inplace=True)
+qntd_assinalas_materia = qntd_assinaladas[qntd_assinaladas['Disciplina'] == materia_escolhida]
+
+qntd_assinalas_materia.set_index("Questão",inplace=True)
+st.write(pd.DataFrame({
+    "Gabarito": qntd_assinalas_materia['alternativas_certas'].str.strip('[]'),
+    "1": qntd_assinalas_materia['1'],
+    '2': qntd_assinalas_materia['2'],
+    '4': qntd_assinalas_materia['4'],
+    "8": qntd_assinalas_materia['8'],
+    '16': qntd_assinalas_materia['16'],
+    '32': qntd_assinalas_materia['32'],
+    '64': qntd_assinalas_materia['64']
+}))
+
+# destrinchando as zonas de pontuação
+
--- relatorios/simulinho-html-alunos.py
+++ relatorios/simulinho-html-alunos.py
@@ -2,36 +2,72 @@ import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 from flask import url_for
 import numpy as np
+from os import getcwd
 
 
 # funções pra colorir as tabelas
+
 def _color_correction(data):
     df = data.copy()
-    df.loc[df['Sua resposta'] == df['Gabarito'], :] = 'background-color: lightgreen'
-    df.loc[df['Sua resposta'] != df['Gabarito'], :] = 'background-color: #d48585'
+    n = len(df)
+    
+    df.loc[df['Pontuação'] == "1.0", :] = 'background-color: #0fa92c'
+    df.loc[df['Pontuação'] == "0.0", :] = 'background-color: #d48585'
+
     return df
 
 def _color_table_every_other(data):
     df = data.copy()
     n = len(df)
-    df.iloc[range(0,n,2), :] = 'background-color: #f5f5dc'
-    df.iloc[range(1,n,2), :] = 'background-color: #f0f0ce'
-    df.iloc[-1,:] += '; font-weight: bold'
+    df.iloc[range(0,n,1), :] = 'background-color: #353F70'
+    df.iloc[range(0,n,1), :] = 'color: white'
+
+    #df.iloc[range(0,n,1), :] = 'opacity: 1'
     return df
 
 def _color_table_redacao(data):
     df = data.copy()
     n = len(df)
     # print(df.iloc[0:5,0:3])
-    df.iloc[0:6,0:3] = 'background-color: #f0f0ce'
+    df.iloc[0:6,0:3] = 'background-color: #353F70'
     df.iloc[-1] += '; font-weight: bold'
+    #df.iloc[range(0,n,1), :] = 'opacity: 1'
+    df.iloc[range(0,n,1), :] = 'color: white'
+
     return df
 
+# carrega os DataFrames com os dados de questoes
+df_q = pd.read_pickle('relatorios/dados/aluno/pontuacao_por_questao.pkl')
+df_q = df_q.rename(columns={'assinaladas':'Sua resposta', 'alternativas_certas':'Gabarito','pontuacao':'Pontuação'})
+#df_q['Sua resposta'] = df_q['Sua resposta'].str.upper()
+#df_q['Gabarito'] = df_q['Gabarito'].str.upper()
+
+# notas dos alunos por materia
+df_n = pd.read_pickle('relatorios/dados/aluno/pontuacao_por_materia.pkl')
+df_n['Média Individual'] = df_n['Média Individual'].apply(lambda x: str(int(x*100))+'%')
+df_n['Média Geral'] = df_n['Média Geral'].apply(lambda x: str(int(x*100))+'%')
 
 
 # dados da redacao
-df_r = pd.read_pickle('redacao.pkl')
+df_r = pd.read_pickle('relatorios/dados/aluno/redacao.pkl')
 
+
+# dados das discursivas
+
+df_d = pd.read_pickle('relatorios/dados/aluno/discursivas_alunos.pkl')
+df_d.fillna("",inplace=True)
+
+# todas as notas
+df_total = pd.read_pickle('relatorios/dados/aluno/pontuacao_total.pkl')
+
+
+# colocação dos alunos
+colocacao = pd.read_pickle("relatorios\dados\docentes\colocacao.pkl")
+
+# dados pessoasi
+
+df_p = pd.read_excel("dados_2021.1\dados_pessoais.xlsx")
+df_p.set_index("Login Eduqo", inplace=True)
 
 relatorio_alunos = {}
 
@@ -39,53 +75,138 @@ relatorio_alunos = {}
 # /!\ IMPORTANTE /!\
 # aqui carrega o template HTML que vai ser usado pelo Jinja
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template("relatorios/simulinho_aluno_template.html")
+template = env.get_template("relatorios/simufsc.html")
 
 # cria um dicionário com as variáveis pro template
 doc = {
-    'document_title': 'Correção SIMULINHO 2021'
+    'document_title': 'Correção SIMUFSC 2021'
 }
 
 # pra cada aluno
-for aluno in df_r.index.get_level_values(0).unique():
+for aluno in df_q.index.get_level_values(0).unique():
     # print(aluno)
 
-    # a cada iteração, ele pega os dados correspondente ao 'aluno'
-    doc['nome'] = aluno # nome do aluno
-    doc['materias'] = []
     print(aluno)
+    # a cada iteração, ele pega os dados correspondente ao 'aluno'
+     # nome do aluno
+    doc['materias'] = []
 
-    # dados pessoais de cada aluno
 
-    # redacao
+    # nome de cada aluno
+    if aluno in df_p.index:
+        nome = df_p.loc[aluno, :]
+        nome = nome.Aluno
+        doc['nome'] = nome
+    else: 
+        nome = aluno
+    # colocacao de cada aluno
+    coloc_aluno = colocacao[colocacao["aluno_login"] == aluno]
 
-    red_aluno = df_r.loc[aluno, :]
-    # print(len(red_aluno))
-    print(red_aluno)
-    
-    red_aluno["Criterio"] = red_aluno.index.get_level_values(0)
-    red_aluno.loc[:, 'Nota'] = (red_aluno['Nota']).astype(str)
-    red_aluno.loc[:, 'Comentario'] = red_aluno['Comentário']
-    
-    red_aluno= red_aluno[['Criterio', 'Nota','Comentário']]
-    
-    
-    # print(red_aluno)
-    doc['redacao'] = red_aluno.style.apply(
-                _color_table_redacao,
+    coloc_aluno.loc[:, 'Colocação'] = coloc_aluno['Colocação']
+        
+    doc['colocacao'] = coloc_aluno.Colocação.iat[-1]
+
+
+    # pontuacao geral de cada aluno
+    pont_aluno = df_total.loc[df_total['aluno_login'] == aluno]
+
+    pont_aluno =   round(pont_aluno[['Prova objetiva','Questões discursivas',  'Redação']], 2).astype(str)
+    #print(pont_aluno)
+    doc['pontuacao'] = pont_aluno.style.apply(
+                _color_table_every_other,
                 axis=None
-            ).set_properties(
-                **{'text-align':'center',
-                   'font-family':'Roboto',
-                   'border-color':'black',
-                   'border-style' :'solid',
-                   'border-width': '1px',
-                   'border-collapse':'collapse'}
-            ).set_table_styles(
-                [{'props':[('color','#533884'), ('font-family','Roboto')]}]
             ).hide_index().render()
+
+
+    # questoes de cada aluno
+    df_q_aluno = df_q.loc[aluno, :]
+    
+    #df_q_aluno['Questão'] = df_q_aluno.index.get_level_values(1)
+    #print(df_q_aluno['Pontuação'])
+    df_q_aluno = df_q_aluno[['Sua resposta','Gabarito','TEMA', 'NÍVEL',"Pontuação"]]
+
+    # pra cada matéria:
+    for materia in df_q_aluno.index.get_level_values(0).unique():
+        
+        doc['materias'].append({
+            'materia':materia,
+            'df':df_q_aluno.loc[materia, :].style.apply(
+                _color_correction,
+                axis=None
+            ).hide_index().render()
+        })
+
+    # mostrar a pontuação em cada materia
+
+    df_n_aluno = df_n.loc[aluno, :]
+    df_n_aluno['Matéria'] = df_n_aluno.index.get_level_values(0)
+    df_n_aluno.loc[:, 'Pontuação'] = df_n_aluno['Pontuação']
+    df_n_aluno.loc[:, 'Média Individual'] = df_n_aluno['Média Individual']
+    df_n_aluno.loc[:, 'Média Geral'] = df_n_aluno['Média Geral']
+    
+    df_n_aluno = df_n_aluno[['Matéria','Pontuação', 'Média Individual',  'Média Geral']]
+
+    doc['notas'] = df_n_aluno.style.apply(
+                _color_table_every_other,
+                axis=None
+            ).hide_index().render()
+
+    
+    # pode ser que alguns alunos façam a redação e não façam a prova objetiva, e vice-versa
+    if aluno in df_r.index:
+        
+        red_aluno = df_r.loc[aluno, :]
+        # print(len(red_aluno))
+        #print(red_aluno)
+        
+        red_aluno["Criterio"] = red_aluno.index.get_level_values(0)
+        red_aluno.loc[:, 'Nota'] = (red_aluno['Nota']).astype(str)
+        red_aluno.loc[:, 'Comentario'] = red_aluno['Comentário']
+        
+        red_aluno = red_aluno[['Criterio', 'Nota','Comentário']]
+        
+        
+        #print(red_aluno)
+        doc['redacao'] = red_aluno.style.apply(
+                    _color_table_redacao,
+                    axis=None
+                ).hide_index().render()
+
+    else:
+        doc['redacao'] = 'Não fez a redação'
+        doc['comentario'] = []
+
+  
+    # discursivas
+
+    if aluno in df_d.index:
+        
+        disc_aluno = df_d.loc[aluno, :]
+        # print(len(red_aluno))
+        #print(red_aluno)
+        disc_aluno["Matéria"] = disc_aluno.index.get_level_values(0)
+        disc_aluno.loc[:, 'a)'] = (disc_aluno['a)']).astype(str)
+        disc_aluno.loc[:, 'b)'] = (disc_aluno['b)']).astype(str)
+        disc_aluno.loc[:, 'c)'] = (disc_aluno['c)']).astype(str)
+        disc_aluno.loc[:, 'Nota final'] = (disc_aluno['Nota final']).astype(str)
+        disc_aluno.loc[:, 'Comentários'] = disc_aluno['Comentários']
+        
+        disc_aluno= disc_aluno[["Matéria", 'a)',	'b)','c)','Nota final','Comentários']]
+        
+        
+        # print(red_aluno)
+        doc['discursivas'] = disc_aluno.style.apply(
+                    _color_table_every_other,
+                    axis=None
+                ).hide_index().render()
+
+    else:
+        doc['discursivas'] = 'Não fez as discursivas'
+        doc['comentario'] = []
+
+    doc['grafico_path'] = f'{getcwd()}/relatorios/graficos/{aluno}.jpg'
 
     # renderiza o html a partir do template e com as informações do dicionário
     html_out = template.render(doc)
-    with open(f"html-alunos/{aluno.replace(' ', '_')}.html",'w') as file:
+    with open(f"html-alunos/{nome.replace(' ', '_')}.html",'w',  encoding='utf-8') as file:
         file.write(html_out)
